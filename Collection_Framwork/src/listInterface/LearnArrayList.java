e4b099d4aec19210f9886167aa5462d0b122e669
bandriya <115407277+bandriya@users.noreply.github.com>
2023-07-14 22:31:44 +0530

    ArrayList

---
+++ Collection_Framwork/src/listInterface/LearnArrayList.java
@@ -0,0 +1,50 @@
+package listInterface;
+
+import java.util.ArrayList;
+
+public class LearnArrayList {
+	public static void main(String[] args) {
+//	String studentName[]=new String[30];
+//	studentName[0]="mahi";
+//	//
+//	//
+//	//
+//	studentName[29]="mukesh";
+		
+//		ArrayList<Integer>list=new ArrayList<>();
+//		list.add(1);
+//		list.add(2);
+//		list.add(3);
+//		list.add(4);
+//		ArrayList<Integer>newlist=new ArrayList<>();
+//		newlist.add(80);
+//		newlist.add(90);
+//		System.out.println(list);
+//		list.add(5);
+//		System.out.println(list);
+//		list.add(1,50);
+//		System.out.println(list);
+//		list.addAll(newlist);
+//		System.out.println(list);
+//		System.out.println(list.get(1));
+//		list.remove(Integer.valueOf(90));
+//		System.out.println(list);
+//		list.clear();
+//		System.out.println(list);
+		
+		ArrayList<Integer>list=new ArrayList<>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+//		for(int i=0;i<list.size();i++) {
+//			System.out.println(list.get(i));
+//		}
+		
+//		for(Integer a:list) {
+//			System.out.print(a+" ");
+//		}
+		
+		
+		
+	}
+}
